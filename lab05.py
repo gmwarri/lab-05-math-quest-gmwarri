f820ba3c795afb8130ccc793cee8ff626f4531cc
gmwarri <gmwarri@clemson.edu>
2026-02-15 22:44:56 -0500

    first commit

--- lab05.py
+++ lab05.py
@@ -15,7 +15,16 @@ def generate_problem(operation: str, difficulty: int) -> int:
     Returns:
         Prints out a random math problem difficulty
     """
-    
+    if operation == "addition":
+        symbol = "+"
+    elif operation == "subtraction":
+        symbol = "-"
+    elif operation == "multiplication":
+        symbol = "*"
+    elif operation == "division":
+        symbol = "/"
+
+
     # This will generate a random first and second number for you to use
     first_num = random.randint(1, (10 ** difficulty))
     second_num = random.randint(1, (10 ** difficulty))
@@ -25,39 +34,96 @@ def generate_problem(operation: str, difficulty: int) -> int:
     second_num = min([first_num, second_num])
 
     # eval() returns the solution to your problem.
-    int(eval(f"{first_num} {operation} {second_num}"))
+    answer = int(eval(f"{first_num} {symbol} {second_num}"))
 
     #TODO: Print the operation and return the solution.
+    print(f"{first_num} {symbol} {second_num} = ?")
+
+    return answer
+
+def get_valid_operation():
+    valid_ops = ("addition", "subtraction", "multiplication", "division")
+
+    operation = input().strip()
+
+
+    while operation not in valid_ops: 
+        print("Please select either addition, subtraction, multiplication, or division")
+        operation = input().strip()
+        
+    return operation
+
+
+
+def get_valid_problem_count(operation):
+    print(f"How many types of {operation} problems would you like to solve?")
+    
+    num_problems = int(input().strip())
+    
+    #stop non-common operations
+    while num_problems <= 0: 
+        print("Please enter a number greater than 0!")
+        num_problems = int(input().strip())
+        
+    return num_problems
+    
 
 # ---------START CODING HERE----------
+if __name__ == "__main__":
+    
+    print("Welcome to Math Quest! Here you will be challenged by answering increasingly difficult math problems until you decide you have had enough.")
+    print('You will be presented with n number math problems at a time. If you get more than half right, we will increase the difficulty. Otherwise, we will lower the difficulty, if possible.')    
 
-print("Welcome to Math Quest! Here you will be challenged by answering increasingly difficult math problems until you decide you have had enough.")
-print('You will be presented with n number math problems at a time. If you get more than half right, we will increase the difficulty. Otherwise, we will lower the difficulty, if possible.')
+    difficulty = 1 # starts at 1
+    temp_score = 0
+    total_questions = 0
+    cont = "yep"
+    final_score = 0
 
-print("Would you like to practice addition, subtraction, multiplication, or division?")
-print("Please select either addition, subtraction, multiplication, or division")
+    while cont != "quit":
+        print("Would you like to practice addition, subtraction, multiplication, or division?")
+        operation = get_valid_operation()
 
-print(f"How many types of {operation} problems would you like to solve?")
-print("Please enter a number greater than 0!")
+        num_problems = get_valid_problem_count(operation)
 
-print(f"Here are your {num_problems} {operation} problems:")
-answer = generate_problem(operation, difficulty)
+        print(f"Here are your {num_problems} {operation} problems:")
 
-print("Correct! Next problem...")
-print("(loud incorrect noise) Wrong! Next problem...")
+        temp_score = 0        
 
-print(f"Your score was {temp_score}/{num_problems}. We will be increasing the difficulty for next time!")
-print(f"Your score was {temp_score}/{num_problems}. We will be lowering the difficulty for next time.")
-print("You are already at the lowest difficulty!")
+    
+        for i in range(num_problems):
+            print(f"Problem {i + 1} of {num_problems}")
+            
+            answer = generate_problem(operation, difficulty)
+            user_answer = input("Answer: ").strip()
 
-print("Continue? (enter 'quit' to exit)")
-
-
-print(f"Congrats! Your final score was {final_score} out of {questions}.")
-
-
-
-
-
+            if user_answer == str(answer):
+                print("Correct! Next problem...")
+                temp_score += 1
 
 
+            else:
+                print("(loud incorrect noise) Wrong! Next problem...")
+
+
+
+        final_score += temp_score
+        total_questions += num_problems
+
+        if temp_score > num_problems / 2:
+            difficulty += 1
+            print(f"Your score was {temp_score}/{num_problems}. We will be increasing the difficulty for next time!")
+        else:
+            if difficulty > 1:
+                difficulty -= 1
+                print(f"Your score was {temp_score}/{num_problems}. We will be lowering the difficulty for next time.")
+            else:
+                print("You are already at the lowest difficulty!")
+
+
+        print("Continue? (enter 'quit' to exit)")
+        cont = input().strip()   
+
+
+
+    print(f"Congrats! Your final score was {final_score} out of {total_questions}.")
